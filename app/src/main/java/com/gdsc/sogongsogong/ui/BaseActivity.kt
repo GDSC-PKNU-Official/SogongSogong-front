288e4d9d85523f6f426fff73be789f5a354e25e5
차지원 <chajiwon100785@gmail.com>
2022-02-18 13:47:34 +0900

    feat: base activity, fragment, viewholder for databinding

---
+++ app/src/main/java/com/gdsc/sogongsogong/ui/BaseActivity.kt
@@ -0,0 +1,18 @@
+package com.gdsc.sogongsogong.ui
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+
+open class BaseActivity<VDB: ViewDataBinding>(val layoutId: Int): AppCompatActivity() {
+
+    lateinit var binding: VDB
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+        binding = DataBindingUtil.setContentView(this, layoutId)
+        binding.lifecycleOwner = this
+    }
+}
---
+++ app/src/main/java/com/gdsc/sogongsogong/ui/BaseFragment.kt
@@ -0,0 +1,36 @@
+package com.gdsc.sogongsogong.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.fragment.app.Fragment
+import java.lang.IllegalStateException
+
+open class BaseFragment<VDB: ViewDataBinding>(private val layoutId: Int): Fragment() {
+
+    private var _binding: VDB? = null
+    val binding get() = _binding ?: throw IllegalStateException(ERROR_BINDING_INITIALIZE)
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        _binding = DataBindingUtil.inflate(inflater, layoutId, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.lifecycleOwner = viewLifecycleOwner
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    companion object {
+
+        private const val ERROR_BINDING_INITIALIZE = "binding initialize error"
+    }
+}
