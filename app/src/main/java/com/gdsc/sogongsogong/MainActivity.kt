7b8bd1effcdfef1e309d4264a51627a15887d030
차지원 <chajiwon100785@gmail.com>
2022-02-18 14:19:13 +0900

    feat: set main activity

---
+++ app/src/main/java/com/gdsc/sogongsogong/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.gdsc.sogongsogong
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import com.gdsc.sogongsogong.databinding.ActivityMainBinding
+import com.gdsc.sogongsogong.ui.BaseActivity
+
+class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+
+        setInitialFragment()
+    }
+
+    private fun setInitialFragment() {
+        // TODO: set navHostFragment
+    }
+}
