681fac03c4cdef4634413d1379d832a0d3ab8971
차지원 <chajiwon100785@gmail.com>
2022-02-19 14:37:16 +0900

    feat: setting page example

---
+++ app/src/main/java/com/gdsc/sogongsogong/ui/setting/SettingFragment.kt
@@ -0,0 +1,23 @@
+package com.gdsc.sogongsogong.ui.setting
+
+import android.os.Bundle
+import android.view.View
+import com.gdsc.sogongsogong.R
+import com.gdsc.sogongsogong.databinding.FragmentSettingBinding
+import com.gdsc.sogongsogong.ui.BaseFragment
+
+class SettingFragment : BaseFragment<FragmentSettingBinding>(R.layout.fragment_setting) {
+
+    private lateinit var adapter: SettingContentsAdapter
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setAdapter()
+    }
+
+    private fun setAdapter() {
+        adapter = SettingContentsAdapter()
+        binding.rvSettingContents.adapter = adapter
+    }
+}
