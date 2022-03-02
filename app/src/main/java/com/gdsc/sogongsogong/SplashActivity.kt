45f4448cd852d6d7f14738260ef969b47139e21b
cyun-404 <73474866+cyun-404@users.noreply.github.com>
2022-03-03 00:25:00 +0900

    spalsh 동작 수정

--- app/src/main/java/com/gdsc/sogongsogong/SplashActivity.kt
+++ app/src/main/java/com/gdsc/sogongsogong/SplashActivity.kt
@@ -4,20 +4,26 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
-import com.gdsc.sogongsogong.MainActivity
 
 class SplashActivity : AppCompatActivity() {
-
-    // 2초간 스플래시 화면 보여줌 (ms)
-    private val SPLASH_VIEW_TIME: Long = 2000
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
         Handler().postDelayed({
-            startActivity(Intent(this, MainActivity::class.java))
+            val intent = Intent(this, MainActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
+            startActivity(intent)
             finish()
-        }, SPLASH_VIEW_TIME)
+        },DURATION)
+
+    }
+    companion object {
+        private const val DURATION : Long = 2000
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
     }
 }
