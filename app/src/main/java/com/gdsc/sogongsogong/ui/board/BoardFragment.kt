593ceec36382631463f1c38e71676138bf52777f
차지원 <chajiwon100785@gmail.com>
2022-03-06 12:28:46 +0900

    Issue #15 feat: collect recyclerView click event

--- app/src/main/java/com/gdsc/sogongsogong/ui/board/BoardFragment.kt
+++ app/src/main/java/com/gdsc/sogongsogong/ui/board/BoardFragment.kt
@@ -2,22 +2,46 @@ package com.gdsc.sogongsogong.ui.board
 
 import android.os.Bundle
 import android.view.View
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.navGraphViewModels
 import com.gdsc.sogongsogong.R
 import com.gdsc.sogongsogong.databinding.FragmentBaseBoardBinding
 import com.gdsc.sogongsogong.ui.base.BaseFragment
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class BoardFragment: BaseFragment<FragmentBaseBoardBinding>(R.layout.fragment_base_board) {
 
+    private lateinit var adapter: BoardAdapter
+
+    private val boardViewModel: BoardViewModel by navGraphViewModels(R.layout.fragment_base_board) { defaultViewModelProviderFactory }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         setAdapter()
+        setCoroutine()
     }
 
     private fun setAdapter() {
-        // TODO: set adapter
+        binding.rvBaseBoardBoard.adapter = adapter
+    }
+
+    private suspend fun collectRecyclerView() {
+        boardViewModel.recyclerViewClickEvent.collect {
+            navigatePost()
+        }
+    }
+
+    private fun navigatePost() {
+        // TODO: 전달받은 게시글로 이동
+    }
+
+    private fun setCoroutine() {
+        viewLifecycleOwner.lifecycleScope.launch {
+            collectRecyclerView()
+        }
     }
 }
