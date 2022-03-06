36762bc540762304a184804f577fbbbdebc24b91
차지원 <chajiwon100785@gmail.com>
2022-03-06 12:27:38 +0900

    Issue #15 feat: set recyclerView click event

--- app/src/main/java/com/gdsc/sogongsogong/ui/board/BoardViewModel.kt
+++ app/src/main/java/com/gdsc/sogongsogong/ui/board/BoardViewModel.kt
@@ -3,6 +3,11 @@ package com.gdsc.sogongsogong.ui.board
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.launch
 
 class BoardViewModel : ViewModel() {
 
@@ -11,11 +16,20 @@ class BoardViewModel : ViewModel() {
     private var _board: MutableLiveData<String> = MutableLiveData() // FIXME: entity로 변경
     val board: LiveData<String> = _board
 
+    private val _recyclerViewClickEvent = MutableSharedFlow<Unit>()
+    val recyclerViewClickEvent: SharedFlow<Unit> = _recyclerViewClickEvent
+
     fun fetchBoard(page: Int) {
         // TODO: return repository.fetchBoard(page)
     }
 
     fun fetchBoard() {
         // TODO: return repository.fetchBoard()
+    }
+
+    fun emitRecyclerViewClickEvent() {
+        viewModelScope.launch {
+            _recyclerViewClickEvent.emit(Unit)
+        }
     }
 }
