455fbf534e0a907576e71e30b04818faf725eb68
차지원 <chajiwon100785@gmail.com>
2022-03-10 06:34:50 +0900

    Issue #16 test: set fake dispatcher provider for test

---
+++ app/src/test/java/com/gdsc/sogongsogong/FakeDispatcherProvider.kt
@@ -0,0 +1,16 @@
+package com.gdsc.sogongsogong
+
+import com.gdsc.sogongsogong.di.dispatcher.DispatcherProvider
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.newSingleThreadContext
+
+@DelicateCoroutinesApi
+class FakeDispatcherProvider : DispatcherProvider {
+    override val main: CoroutineDispatcher
+        get() = newSingleThreadContext("Main thread")
+    override val io: CoroutineDispatcher
+        get() = newSingleThreadContext("Io thread")
+    override val default: CoroutineDispatcher
+        get() = newSingleThreadContext("Default thread")
+}
