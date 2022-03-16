4b0144cbd28fed4048bb62394b1137d002fb8432
차지원 <chajiwon100785@gmail.com>
2022-03-16 15:14:18 +0900

    Issue #14 fix: conflict 해결

--- app/src/main/java/com/gdsc/sogongsogong/MainActivity.kt
+++ app/src/main/java/com/gdsc/sogongsogong/MainActivity.kt
@@ -2,6 +2,11 @@ package com.gdsc.sogongsogong
 
 import android.os.Bundle
 import android.os.PersistableBundle
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.gdsc.sogongsogong.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 import com.gdsc.sogongsogong.ui.base.BaseActivity
@@ -12,10 +17,23 @@ class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setInitialFragment()
+//        setInitialFragment()
+//        setBottomNav()
     }
 
     private fun setInitialFragment() {
-        // TODO: set navHostFragment
+        val navHost = NavHostFragment.create(R.navigation.nav_graph)
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.homeFragment, navHost)
+            .setPrimaryNavigationFragment(navHost)
+            .commit()
+    }
+
+    private fun setBottomNav() {
+        val navController = findNavController(R.id.nav_graph_bottom_nav)
+        val appBarConfiguration = AppBarConfiguration(setOf(R.id.homeFragment, R.id.postFragment, R.id.settingFragment))
+
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        binding.navMainBottomNav.setupWithNavController(navController)
     }
 }
