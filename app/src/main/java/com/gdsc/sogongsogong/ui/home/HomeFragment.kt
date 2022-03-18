467d035b626628f389c81a8f8631bbf1789d9e6a
차지원 <chajiwon100785@gmail.com>
2022-03-18 17:31:01 +0900

    Issue #39 feat: home fragment에 정보rv 연결

--- app/src/main/java/com/gdsc/sogongsogong/ui/home/HomeFragment.kt
+++ app/src/main/java/com/gdsc/sogongsogong/ui/home/HomeFragment.kt
@@ -1,9 +1,30 @@
 package com.gdsc.sogongsogong.ui.home
 
+import android.os.Bundle
+import android.view.View
 import com.gdsc.sogongsogong.R
 import com.gdsc.sogongsogong.databinding.FragmentHomeBinding
 import com.gdsc.sogongsogong.ui.base.BaseFragment
 
 class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
 
+    private val informationAdapter: InformationAdapter by lazy { InformationAdapter() }
+
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initBinding()
+        submitInformationBanner()
+    }
+
+    private fun initBinding() {
+        binding.rvHomeInformation.adapter = informationAdapter
+    }
+
+    private fun submitInformationBanner() {
+        // FIXME: submit real list
+        informationAdapter.submitList(listOf(1, 2, 3, 4, 5, 6, 7))
+    }
 }
