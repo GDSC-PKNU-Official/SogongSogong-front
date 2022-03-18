f05d933c1ead4c8f92645e197e841aa3992b5b85
차지원 <chajiwon100785@gmail.com>
2022-03-18 16:51:03 +0900

    Issue #39 feat: add home fragment

---
+++ app/src/main/java/com/gdsc/sogongsogong/ui/home/HomeFragment.kt
@@ -0,0 +1,9 @@
+package com.gdsc.sogongsogong.ui.home
+
+import com.gdsc.sogongsogong.R
+import com.gdsc.sogongsogong.databinding.FragmentHomeBinding
+import com.gdsc.sogongsogong.ui.base.BaseFragment
+
+class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
+
+}
