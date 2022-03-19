de57184d3bd8f0d66e919e868a436c428fc288a5
차지원 <chajiwon100785@gmail.com>
2022-03-19 11:59:40 +0900

    Issue #39 test: 임시값 팩토리 생성

---
+++ app/src/main/java/com/gdsc/sogongsogong/fake/FakeFactory.kt
@@ -0,0 +1,64 @@
+package com.gdsc.sogongsogong.fake
+
+import com.gdsc.sogongsogong.data.entity.Post
+
+object FakeFactory {
+    fun getFakePosts(): List<Post> = listOf(
+        Post(
+            boardId = 0,
+            title = "제목제목",
+            body = "컨텐츠내용",
+            userId = "1",
+            regdate = "17:50",
+            type = "",
+            goodCount = 30,
+            commentCount = 12,
+            scrapCount = 10,
+            goodCheck = "",
+            scrapCheck = "",
+            userCheck = ""
+        ),
+        Post(
+            boardId = 0,
+            title = "제목제목",
+            body = "컨텐츠내용",
+            userId = "1",
+            regdate = "17:50",
+            type = "",
+            goodCount = 30,
+            commentCount = 12,
+            scrapCount = 10,
+            goodCheck = "",
+            scrapCheck = "",
+            userCheck = ""
+        ),
+        Post(
+            boardId = 0,
+            title = "제목제목",
+            body = "컨텐츠내용",
+            userId = "1",
+            regdate = "17:50",
+            type = "",
+            goodCount = 30,
+            commentCount = 12,
+            scrapCount = 10,
+            goodCheck = "",
+            scrapCheck = "",
+            userCheck = ""
+        ),
+        Post(
+            boardId = 0,
+            title = "제목제목",
+            body = "컨텐츠내용",
+            userId = "1",
+            regdate = "17:50",
+            type = "",
+            goodCount = 30,
+            commentCount = 12,
+            scrapCount = 10,
+            goodCheck = "",
+            scrapCheck = "",
+            userCheck = ""
+        )
+    )
+}
