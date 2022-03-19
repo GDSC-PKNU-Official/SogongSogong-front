a3cdc63c67db40a99d49680861a658b725e54a79
차지원 <chajiwon100785@gmail.com>
2022-03-19 11:59:58 +0900

    Issue #39 feat: 홈화면 하단 게시판 리사이클러뷰 어댑터 생성

---
+++ app/src/main/java/com/gdsc/sogongsogong/ui/home/HomeBoardAdapter.kt
@@ -0,0 +1,30 @@
+package com.gdsc.sogongsogong.ui.home
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import com.gdsc.sogongsogong.R
+import com.gdsc.sogongsogong.data.entity.Post
+import com.gdsc.sogongsogong.databinding.ItemHomeBoardBinding
+import com.gdsc.sogongsogong.ui.base.BaseViewHolder
+
+class HomeBoardAdapter : ListAdapter<Post, BaseViewHolder<ItemHomeBoardBinding>>(diffUtil) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BaseViewHolder<ItemHomeBoardBinding> =
+        BaseViewHolder(parent, R.layout.item_home_board)
+
+    override fun onBindViewHolder(holder: BaseViewHolder<ItemHomeBoardBinding>, position: Int) {
+        holder.binding.item = getItem(position)
+    }
+
+    companion object {
+
+        val diffUtil by lazy {
+            object : DiffUtil.ItemCallback<Post>() {
+                override fun areContentsTheSame(oldItem: Post, newItem: Post) = oldItem.boardId == newItem.boardId
+
+                override fun areItemsTheSame(oldItem: Post, newItem: Post) = oldItem == newItem
+            }
+        }
+    }
+}
