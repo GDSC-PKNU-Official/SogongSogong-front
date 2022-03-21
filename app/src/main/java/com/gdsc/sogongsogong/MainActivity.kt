6c6190d2f55a5102350688c49343f2354d8a3a43
차지원 <chajiwon100785@gmail.com>
2022-03-21 19:22:30 +0900

    Issue #46 fix: navigation  필요없는 코드 제거

--- app/src/main/java/com/gdsc/sogongsogong/MainActivity.kt
+++ app/src/main/java/com/gdsc/sogongsogong/MainActivity.kt
@@ -1,12 +1,7 @@
 package com.gdsc.sogongsogong
 
 import android.os.Bundle
-import android.os.PersistableBundle
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
 import com.gdsc.sogongsogong.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 import com.gdsc.sogongsogong.ui.base.BaseActivity
@@ -17,22 +12,14 @@ class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setInitialFragment()
+        setNavHostFragment()
     }
 
-    private fun setInitialFragment() {
+    private fun setNavHostFragment() {
         val navHost = NavHostFragment.create(R.navigation.nav_graph_bottom_nav)
         supportFragmentManager.beginTransaction()
             .replace(R.id.fragment_main, navHost)
             .setPrimaryNavigationFragment(navHost)
             .commit()
-
-        // TODO: 홈버튼, 글작성버튼, 마이페이지 버튼 when 분기
-        binding.navMainBottomNav.setOnItemSelectedListener { item ->
-            when(item.itemId) {
-                R.menu.bottom_nav_menu -> {true}
-                else -> {true}
-            }
-        }
     }
 }
