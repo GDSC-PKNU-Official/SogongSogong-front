f755a58ab4b7cbbd56b5010a7967210668275e1b
cyun-404 <cys20203330@gmail.com>
2022-03-23 19:29:50 +0900

    issue #63

---
+++ app/src/main/java/com/gdsc/sogongsogong/ui/notification/NotificationFragment.kt
@@ -0,0 +1,8 @@
+package com.gdsc.sogongsogong.ui.notification
+
+import com.gdsc.sogongsogong.R
+import com.gdsc.sogongsogong.databinding.FragmentBoardListBinding
+import com.gdsc.sogongsogong.ui.base.BaseFragment
+
+class NotificationFragment:BaseFragment<FragmentBoardListBinding>(R.layout.fragment_board_list){
+}
