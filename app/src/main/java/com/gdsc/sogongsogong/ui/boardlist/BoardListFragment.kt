cf35b77764ec1d2637aa9c99f392438e56377e29
차지원 <chajiwon100785@gmail.com>
2022-03-24 21:16:20 +0900

    Issue #64 feat: 게시판 목록 -> 게시판 이동 기능 구현

--- app/src/main/java/com/gdsc/sogongsogong/ui/boardlist/BoardListFragment.kt
+++ app/src/main/java/com/gdsc/sogongsogong/ui/boardlist/BoardListFragment.kt
@@ -1,9 +1,43 @@
 package com.gdsc.sogongsogong.ui.boardlist
 
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
+import com.gdsc.sogongsogong.NavViewModel
 import com.gdsc.sogongsogong.R
 import com.gdsc.sogongsogong.databinding.FragmentBoardListBinding
 import com.gdsc.sogongsogong.ui.base.BaseFragment
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
+@AndroidEntryPoint
 class BoardListFragment: BaseFragment<FragmentBoardListBinding>(R.layout.fragment_board_list) {
 
+    private val navViewModel: NavViewModel by viewModels { defaultViewModelProviderFactory }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setBinding()
+        setCoroutine()
+    }
+
+    private fun setBinding() {
+        binding.navViewModel = navViewModel
+    }
+
+    private fun setCoroutine() {
+        lifecycleScope.launch {
+            collectBoardClickEvent()
+        }
+    }
+    
+    private suspend fun collectBoardClickEvent() {
+        navViewModel.boardClickEvent.collect {
+            findNavController().navigate(R.id.action_boardList_to_board)
+        }
+    }
 }
