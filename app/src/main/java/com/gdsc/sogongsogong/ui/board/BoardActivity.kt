2475fb5557825348d87dcdc6a1768bfe3589c3e3
차지원 <chajiwon100785@gmail.com>
2022-03-29 01:47:58 +0900

    Issue #91 feat: navigate board to writePost

--- app/src/main/java/com/gdsc/sogongsogong/ui/board/BoardActivity.kt
+++ app/src/main/java/com/gdsc/sogongsogong/ui/board/BoardActivity.kt
@@ -1,13 +1,14 @@
 package com.gdsc.sogongsogong.ui.board
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.lifecycle.lifecycleScope
-import androidx.navigation.findNavController
 import com.gdsc.sogongsogong.NavViewModel
 import com.gdsc.sogongsogong.R
 import com.gdsc.sogongsogong.databinding.ActivityBoardBinding
 import com.gdsc.sogongsogong.ui.base.BaseActivity
+import com.gdsc.sogongsogong.ui.writepost.WritePostActivity
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
@@ -38,6 +39,7 @@ class BoardActivity: BaseActivity<ActivityBoardBinding>(R.layout.activity_board)
     private fun setCoroutine() {
         lifecycleScope.launch {
             collectBackButtonClickEvent()
+            collectBoardClickEvent()
         }
     }
 
@@ -45,5 +47,15 @@ class BoardActivity: BaseActivity<ActivityBoardBinding>(R.layout.activity_board)
         navViewModel.backButtonEvent.collect {
             finish()
         }
+    }
+
+    private suspend fun collectBoardClickEvent() {
+        navViewModel.boardClickEvent.collect {
+            showWritePostActivity()
+        }
+    }
+
+    private fun showWritePostActivity() {
+        startActivity(Intent(this, WritePostActivity::class.java))
     }
 }
