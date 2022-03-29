eb97d0badf0835e7f7967c10e6d59f587edbb162
차지원 <chajiwon100785@gmail.com>
2022-03-29 20:27:21 +0900

    Issue #100 fix: class -> object

--- app/src/main/java/com/gdsc/sogongsogong/fake/RetrofitInstance.kt
+++ app/src/main/java/com/gdsc/sogongsogong/fake/RetrofitInstance.kt
@@ -4,7 +4,7 @@ import com.gdsc.sogongsogong.fake.Constants.Companion.BASE_URL
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-class RetrofitInstance {
+object RetrofitInstance {
     private val retrofit by lazy {
         Retrofit.Builder()
             .baseUrl(BASE_URL)
