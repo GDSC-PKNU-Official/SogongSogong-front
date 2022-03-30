8218f88778f98f2bf29363ac43d7e03aed8ca6d9
cyun-404 <73474866+cyun-404@users.noreply.github.com>
2022-03-31 02:21:26 +0900

    Update DtoDatas.kt

--- app/src/main/java/com/gdsc/sogongsogong/fake/DtoDatas.kt
+++ app/src/main/java/com/gdsc/sogongsogong/fake/DtoDatas.kt
@@ -18,6 +18,34 @@ class DtoDatas {
         val postId: Long,
         val category: Boolean
     )
+    data class putPostDto(
+        val postId: Long,
+        val subject: String,
+        val content: String
+    )
+    data class postHashtag2Dto(
+        val postId: Long,
+        val hashName: List<String>
+    )
+    data class putHashtagDto(
+        val postId: Long,
+        val hashName: String
+    )
+    data class postHashtagUser2Dto(
+        val userId: Long,
+        val hashName: String
+    )
+    data class putHashtagUserDto(
+        val userId: Long,
+        val hashName: List<String>
+    )
+
+    data class getUserBusiness2Dto(
+        val num: String,
+        val startDate: String,
+        val pName:String
+    )
+
 
 
 }
