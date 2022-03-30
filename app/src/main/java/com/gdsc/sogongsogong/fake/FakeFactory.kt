2ca174a3d582ead9678d16b48a3cf36f70ace999
차지원 <chajiwon100785@gmail.com>
2022-03-30 14:56:38 +0900

    Issue #109 feat: 추가 알림화면 아이템

--- app/src/main/java/com/gdsc/sogongsogong/fake/FakeFactory.kt
+++ app/src/main/java/com/gdsc/sogongsogong/fake/FakeFactory.kt
@@ -1,5 +1,6 @@
 package com.gdsc.sogongsogong.fake
 
+import com.gdsc.sogongsogong.data.entity.Noti
 import com.gdsc.sogongsogong.data.entity.Post
 
 object FakeFactory {
@@ -60,5 +61,36 @@ object FakeFactory {
             scrapCheck = "",
             userCheck = ""
         )
+    )
+
+    fun getFakeNotifs() = listOf(
+        Noti(
+            title = "한달쯤 걸린다 하더라구요 ㅜㅜ",
+            body = "내용내용",
+            boardId = 0,
+            type = "",
+            regDate = "2022.03.18 20:46"
+        ),
+        Noti(
+            title = "한달쯤 걸린다 하더라구요 ㅜㅜ",
+            body = "내용내용",
+            boardId = 0,
+            type = "",
+            regDate = "2022.03.18 20:46"
+        ),
+        Noti(
+            title = "한달쯤 걸린다 하더라구요 ㅜㅜ",
+            body = "내용내용",
+            boardId = 0,
+            type = "",
+            regDate = "2022.03.18 20:46"
+        ),
+        Noti(
+            title = "한달쯤 걸린다 하더라구요 ㅜㅜ",
+            body = "내용내용",
+            boardId = 0,
+            type = "",
+            regDate = "2022.03.18 20:46"
+        ),
     )
 }
