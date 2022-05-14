1676358ea7926584d1718aa79dc1f8b178038199
차지원 <chajiwon100785@gmail.com>
2022-05-14 20:29:05 +0900

    Issue #131 feat: retrofit 관련 의존성 주입

---
+++ app/src/main/java/com/gdsc/sogongsogong/di/module/RetrofitModule.kt
@@ -0,0 +1,29 @@
+package com.gdsc.sogongsogong.di.module
+
+import com.google.gson.GsonBuilder
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object RetrofitModule {
+
+    private const val BASE_URL = "http://34.82.10.241:8080/"
+
+    private val gson = GsonBuilder().setLenient()
+        .create()
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(): Retrofit =
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .build()
+
+}
