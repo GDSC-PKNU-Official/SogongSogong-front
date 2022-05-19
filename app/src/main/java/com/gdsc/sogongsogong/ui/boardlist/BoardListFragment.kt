1dee7f4e19751b43bed83661465cb63f931dd385
차지원 <chajiwon100785@gmail.com>
2022-05-19 15:43:22 +0900

    Issue #136 feat: board list to hash board 기능 추가

--- app/src/main/java/com/gdsc/sogongsogong/ui/boardlist/BoardListFragment.kt
+++ app/src/main/java/com/gdsc/sogongsogong/ui/boardlist/BoardListFragment.kt
@@ -9,6 +9,7 @@ import com.gdsc.sogongsogong.NavViewModel
 import com.gdsc.sogongsogong.R
 import com.gdsc.sogongsogong.databinding.FragmentBoardListBinding
 import com.gdsc.sogongsogong.ui.base.BaseFragment
+import com.gdsc.sogongsogong.ui.boardlist.BoardType.*
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
@@ -34,10 +35,23 @@ class BoardListFragment: BaseFragment<FragmentBoardListBinding>(R.layout.fragmen
             collectBoardClickEvent()
         }
     }
-    
+
+    // TODO: 게시판 별 이동
     private suspend fun collectBoardClickEvent() {
-        navViewModel.boardClickEvent.collect {
-            findNavController().navigate(R.id.action_boardList_to_board)
+        navViewModel.boardClickEvent.collect { boardType ->
+            findNavController().navigate(
+                when (boardType) {
+                    MY_POST -> R.id.action_boardList_to_board
+                    MY_COMMENT -> R.id.action_boardList_to_board
+                    MY_LIKE -> R.id.action_boardList_to_board
+                    MY_SCRAP -> R.id.action_boardList_to_board
+                    HOT_BOARD -> R.id.action_boardList_to_board
+                    BEST_BOARD -> R.id.action_boardList_to_board
+                    SOGONG_BOARD -> R.id.action_boardList_to_board
+                    HASH_BOARD -> R.id.action_boardList_to_hashBoard
+                    MARKET_BOARD -> R.id.action_boardList_to_board
+                }
+            )
         }
     }
 }
