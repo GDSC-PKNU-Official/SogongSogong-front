8d2090a3f9d2a05aa54c64231576bab34147d00d
차지원 <chajiwon100785@gmail.com>
2022-05-24 14:50:27 +0900

    Issue #145 feat: hot post data source interface 생성

---
+++ app/src/main/java/com/gdsc/sogongsogong/data/datasource/HotPostDataSource.kt
@@ -0,0 +1,9 @@
+package com.gdsc.sogongsogong.data.datasource
+
+import com.gdsc.sogongsogong.data.entity.Post
+import kotlinx.coroutines.flow.Flow
+
+interface HotPostDataSource {
+
+    suspend fun fetchAllHotPosts(): Flow<List<Post>>
+}
