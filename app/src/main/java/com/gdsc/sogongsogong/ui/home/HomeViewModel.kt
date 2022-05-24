e38519e15e91206abbab129b26bbf52a415a852f
차지원 <chajiwon100785@gmail.com>
2022-05-24 14:53:42 +0900

    Issue #145 feat: home 화면이 켜질 때 hot 게시물을 불러온다.

--- app/src/main/java/com/gdsc/sogongsogong/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/gdsc/sogongsogong/ui/home/HomeViewModel.kt
@@ -1,5 +1,6 @@
 package com.gdsc.sogongsogong.ui.home
 
+import com.gdsc.sogongsogong.data.datasource.HotPostDataSource
 import com.gdsc.sogongsogong.data.entity.Post
 import com.gdsc.sogongsogong.data.datasource.PostDataSource
 import com.gdsc.sogongsogong.di.dispatcher.DispatcherProvider
@@ -15,20 +16,31 @@ import javax.inject.Inject
 class HomeViewModel @Inject constructor(
     dispatcherProvider: DispatcherProvider,
     private val postDataSource: PostDataSource,
+    private val hotPostDataSource: HotPostDataSource
 ): BaseViewModel(dispatcherProvider) {
 
     private var _posts: StateFlow<List<Post>> = MutableStateFlow(emptyList())
     val posts: StateFlow<List<Post>> = _posts
 
+    private var _hotPosts: StateFlow<List<Post>> = MutableStateFlow(emptyList())
+    val hotPosts: StateFlow<List<Post>> = _hotPosts
+
     init {
         onIo {
             _posts = postDataSource.fetchInitAllPost()
                 .stateIn(this, SharingStarted.Eagerly, emptyList())
+
+            fetchAllHotPosts()
         }
     }
 
     fun fetchAllPost(postId: Long) = onIo {
         _posts = postDataSource.fetchAllPost(postId)
+            .stateIn(this, SharingStarted.Eagerly, emptyList())
+    }
+
+    private fun fetchAllHotPosts() = onIo {
+        _hotPosts = hotPostDataSource.fetchAllHotPosts()
             .stateIn(this, SharingStarted.Eagerly, emptyList())
     }
 }
